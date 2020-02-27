79d42f8cd60dd0795f430bebc48302b3d601cd80
Checho <checho9999@gmail.com>
2020-02-26 21:13:44 -0300

    Creando el componente Formulario aplicandole estilo con bootswatch; Leyendo y validando desde el Formulario el termino de busqueda ingresado por el usuario; Actualizando lo validado desde el Formulario en el state de la app principal; Creando un componente Error con estilo bootswatch; Creacion de useEffect en la app principal para consultar la API con async y await para obtener las imagenes, en base al termino validado y lo actualizado en el state desde Formulario.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Formulario from './components/Formulario'
 
 function App() {
+
+  //State para recibir la actualizacion del termino de busqueda desde el Formulario
+  const [busqueda, guardarBusqueda] = useState('');
+
+  useEffect(() => {
+
+    const consultarApi = async () => {
+
+      //evitamos la ejecución la primera vez, ya que no hay cambios si busqueda esta vacia
+      if (busqueda === '' ) return;
+
+      //para limitar el numero de imagenes por pagina (la API en su especificacion lo permite)
+      const imagenesPorPagina = 30;
+      //key personal proporcionada por pixabay para acceder a la API 
+      const key = '15401620-bd0aa37e5703af82c20ea43d2';
+      //armamos el string para llevar a cabo la busqueda en base a los parametros dinamicos
+      const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}`;
+    
+      const respuesta = await fetch(url);
+      const resultado = await respuesta.json();
+
+      console.log(resultado.hits);
+
+    }
+
+    consultarApi();
+
+  }, [ busqueda ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className='container'>
+      <div className="jumbotron">
+
+          <p className="lead text-center">Buscador de Imágenes</p>
+
+          <Formulario 
+            guardarBusqueda={guardarBusqueda}
+          />
+
+      </div>
     </div>
   );
+
 }
 
 export default App;
---
+++ src/components/Formulario.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import Error from './Error'
+
+const Formulario = ( { guardarBusqueda } ) => {
+
+    //State para obtener el termino de busqueda ingresado por el usuario
+    const [termino, guardarTermino] = useState('');
+    //State para determinar si el termino de busqueda pasa la validacion
+    const [error, guardarError] = useState(false);
+
+    const buscarImagenes = e => {
+        //para que no se envie el query string en la parte superior, ni se recarge la pagina
+        e.preventDefault();
+
+        //validamos el termino ingresado por el usuario
+        if(termino.trim() === '') {    
+            //ponemos el error en el state de la app principal
+            guardarError(true);
+
+            return;
+        }
+
+        //ponemos el error en el state de la app principal
+        guardarError(false);
+
+        //ponemos el termino en el state de la app principal
+        guardarBusqueda(termino);
+
+    }
+
+    return ( 
+
+        <form
+            onSubmit={buscarImagenes}
+        >
+            <div className="row">
+                <div className="form-group col-md-8">
+                    <input
+                        type="text"
+                        className="form-control form-control-lg"
+                        placeholder="Busca una imagen, ejemplo: futbol o café"
+                        onChange={ e => guardarTermino(e.target.value)}
+                    />
+                </div>
+                <div className="form-group col-md-4">
+                    <input
+                        type="submit"
+                        className="btn btn-lg btn-danger btn-block"
+                        value="Buscar"
+                    />
+                </div>
+            </div>
+
+            { error ? <Error mensaje="Agrega un término de búsqueda" /> : null }
+
+        </form>
+     );
+}
+
+export default Formulario;
